f98a4f35ff1d84080521f8e0f0e116689ec1b6f0
Michael Woolnough <michael.woolnough@gmail.com>
2014-09-11 20:35:37 +0100

    Minor update to default manipulators

--- mw/fmp_manipulation/DefaultFMPManipulators.java
+++ mw/fmp_manipulation/DefaultFMPManipulators.java
@@ -33,17 +33,17 @@ public class DefaultFMPManipulators {
 			case 1:
 				if (number >= Byte.MIN_VALUE && number <= Byte.MAX_VALUE) {
 					data.setByte(tagName, (byte) number);
-					return;
+					break;
 				}
 			case 2:
 				if (number >= Short.MIN_VALUE && number <= Short.MAX_VALUE) {
 					data.setShort(tagName, (short) number);
-					return;
+					break;
 				}
 			case 3:
 				if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) {
 					data.setInteger(tagName, (int) number);
-					return;
+					break;
 				}
 			case 4:
 				data.setLong(tagName, number);
@@ -53,99 +53,99 @@ public class DefaultFMPManipulators {
 	
 	public static class Face extends Multipart {
 
-		private static final byte BOTTOM = 0;
-		private static final byte TOP = 1;
-		private static final byte NORTH = 2;
-		private static final byte SOUTH = 3;
-		private static final byte WEST = 4;
-		private static final byte EAST = 5;
+		protected static final byte BOTTOM = 0;
+		protected static final byte TOP = 1;
+		protected static final byte NORTH = 2;
+		protected static final byte SOUTH = 3;
+		protected static final byte WEST = 4;
+		protected static final byte EAST = 5;
 		
 		protected String tagName = "shape";
 		
 		@Override
 		public void rotate90(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~7);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~7);
 			switch ((byte)(number & 7)) {
 			case NORTH:
-				this.setNumber(tagName, rest | EAST, data);
+				this.setNumber(this.tagName, rest | EAST, data);
 				break;
 			case SOUTH:
-				this.setNumber(tagName, rest | WEST, data);
+				this.setNumber(this.tagName, rest | WEST, data);
 				break;
 			case WEST:
-				this.setNumber(tagName, rest | NORTH, data);
+				this.setNumber(this.tagName, rest | NORTH, data);
 				break;
 			case EAST:
-				this.setNumber(tagName, rest | SOUTH, data);
+				this.setNumber(this.tagName, rest | SOUTH, data);
 				break;
 			}
 		}
 
 		@Override
 		public void rotate180(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~7);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~7);
 			switch ((byte)(number & 7)) {
 			case NORTH:
-				this.setNumber(tagName, rest | SOUTH, data);
+				this.setNumber(this.tagName, rest | SOUTH, data);
 				break;
 			case SOUTH:
-				this.setNumber(tagName, rest | NORTH, data);
+				this.setNumber(this.tagName, rest | NORTH, data);
 				break;
 			case WEST:
-				this.setNumber(tagName, rest | EAST, data);
+				this.setNumber(this.tagName, rest | EAST, data);
 				break;
 			case EAST:
-				this.setNumber(tagName, rest | WEST, data);
+				this.setNumber(this.tagName, rest | WEST, data);
 				break;
 			}
 		}
 
 		@Override
 		public void rotate270(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~7);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~7);
 			switch ((byte)(number & 7)) {
 			case NORTH:
-				this.setNumber(tagName, rest | WEST, data);
+				this.setNumber(this.tagName, rest | WEST, data);
 				break;
 			case SOUTH:
-				this.setNumber(tagName, rest | EAST, data);
+				this.setNumber(this.tagName, rest | EAST, data);
 				break;
 			case WEST:
-				this.setNumber(tagName, rest | SOUTH, data);
+				this.setNumber(this.tagName, rest | SOUTH, data);
 				break;
 			case EAST:
-				this.setNumber(tagName, rest | NORTH, data);
+				this.setNumber(this.tagName, rest | NORTH, data);
 				break;
 			}
 		}
 
 		@Override
 		public void mirrorX(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~7);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~7);
 			switch ((byte)(number & 7)) {
 			case WEST:
-				this.setNumber(tagName, rest | EAST, data);
+				this.setNumber(this.tagName, rest | EAST, data);
 				break;
 			case EAST:
-				this.setNumber(tagName, rest | WEST, data);
+				this.setNumber(this.tagName, rest | WEST, data);
 				break;
 			}
 		}
 
 		@Override
 		public void mirrorZ(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
+			long number = this.getNumber(this.tagName, data);
 			long rest =  number & (~7);
 			switch ((byte)(number & 7)) {
 			case NORTH:
-				this.setNumber(tagName, rest | SOUTH, data);
+				this.setNumber(this.tagName, rest | SOUTH, data);
 				break;
 			case SOUTH:
-				this.setNumber(tagName, rest | NORTH, data);
+				this.setNumber(this.tagName, rest | NORTH, data);
 				break;
 			}
 		}
@@ -161,14 +161,14 @@ public class DefaultFMPManipulators {
 
 		@Override
 		public void rotate90(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~3);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~3);
 			switch ((byte)(number & 3)) {
 			case NS:
-				this.setNumber(tagName, rest | WE, data);
+				this.setNumber(this.tagName, rest | WE, data);
 				break;
 			case WE:
-				this.setNumber(tagName, rest | NS, data);
+				this.setNumber(this.tagName, rest | NS, data);
 				break;
 			}
 		}
@@ -208,196 +208,196 @@ public class DefaultFMPManipulators {
 
 		@Override
 		public void rotate90(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~15);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~15);
 			switch ((byte)(number & 15)) {
 			case Y_NW:
-				this.setNumber(tagName, rest | Y_NE, data);
+				this.setNumber(this.tagName, rest | Y_NE, data);
 				break;
 			case Y_SW:
-				this.setNumber(tagName, rest | Y_NW, data);
+				this.setNumber(this.tagName, rest | Y_NW, data);
 				break;
 			case Y_NE:
-				this.setNumber(tagName, rest | Y_SE, data);
+				this.setNumber(this.tagName, rest | Y_SE, data);
 				break;
 			case Y_SE:
-				this.setNumber(tagName, rest | Y_SW, data);
+				this.setNumber(this.tagName, rest | Y_SW, data);
 				break;
 			case Z_DW:
-				this.setNumber(tagName, rest | X_DN, data);
+				this.setNumber(this.tagName, rest | X_DN, data);
 				break;
 			case Z_DE:
-				this.setNumber(tagName, rest | X_DS, data);
+				this.setNumber(this.tagName, rest | X_DS, data);
 				break;
 			case Z_UW:
-				this.setNumber(tagName, rest | X_UN, data);
+				this.setNumber(this.tagName, rest | X_UN, data);
 				break;
 			case Z_UE:
-				this.setNumber(tagName, rest | X_US, data);
+				this.setNumber(this.tagName, rest | X_US, data);
 				break;
 			case X_DN:
-				this.setNumber(tagName, rest | Z_DE, data);
+				this.setNumber(this.tagName, rest | Z_DE, data);
 				break;
 			case X_DS:
-				this.setNumber(tagName, rest | Z_DW, data);
+				this.setNumber(this.tagName, rest | Z_DW, data);
 				break;
 			case X_UN:
-				this.setNumber(tagName, rest | Z_UE, data);
+				this.setNumber(this.tagName, rest | Z_UE, data);
 				break;
 			case X_US:
-				this.setNumber(tagName, rest | Z_UW, data);
+				this.setNumber(this.tagName, rest | Z_UW, data);
 				break;
 			}
 		}
 
 		@Override
 		public void rotate180(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~15);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~15);
 			switch ((byte)(number & 15)) {
 			case Y_NW:
-				this.setNumber(tagName, rest | Y_SE, data);
+				this.setNumber(this.tagName, rest | Y_SE, data);
 				break;
 			case Y_SW:
-				this.setNumber(tagName, rest | Y_NE, data);
+				this.setNumber(this.tagName, rest | Y_NE, data);
 				break;
 			case Y_NE:
-				this.setNumber(tagName, rest | Y_SW, data);
+				this.setNumber(this.tagName, rest | Y_SW, data);
 				break;
 			case Y_SE:
-				this.setNumber(tagName, rest | Y_NW, data);
+				this.setNumber(this.tagName, rest | Y_NW, data);
 				break;
 			case Z_DW:
-				this.setNumber(tagName, rest | Z_DE, data);
+				this.setNumber(this.tagName, rest | Z_DE, data);
 				break;
 			case Z_DE:
-				this.setNumber(tagName, rest | Z_DW, data);
+				this.setNumber(this.tagName, rest | Z_DW, data);
 				break;
 			case Z_UW:
-				this.setNumber(tagName, rest | Z_UE, data);
+				this.setNumber(this.tagName, rest | Z_UE, data);
 				break;
 			case Z_UE:
-				this.setNumber(tagName, rest | Z_UW, data);
+				this.setNumber(this.tagName, rest | Z_UW, data);
 				break;
 			case X_DN:
-				this.setNumber(tagName, rest | X_DS, data);
+				this.setNumber(this.tagName, rest | X_DS, data);
 				break;
 			case X_DS:
-				this.setNumber(tagName, rest | X_DN, data);
+				this.setNumber(this.tagName, rest | X_DN, data);
 				break;
 			case X_UN:
-				this.setNumber(tagName, rest | X_US, data);
+				this.setNumber(this.tagName, rest | X_US, data);
 				break;
 			case X_US:
-				this.setNumber(tagName, rest | X_UN, data);
+				this.setNumber(this.tagName, rest | X_UN, data);
 				break;
 			}
 		}
 
 		@Override
 		public void rotate270(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~15);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~15);
 			switch ((byte)(number & 15)) {
 			case Y_NW:
-				this.setNumber(tagName, rest | Y_SW, data);
+				this.setNumber(this.tagName, rest | Y_SW, data);
 				break;
 			case Y_SW:
-				this.setNumber(tagName, rest | Y_SE, data);
+				this.setNumber(this.tagName, rest | Y_SE, data);
 				break;
 			case Y_NE:
-				this.setNumber(tagName, rest | Y_NW, data);
+				this.setNumber(this.tagName, rest | Y_NW, data);
 				break;
 			case Y_SE:
-				this.setNumber(tagName, rest | Y_NE, data);
+				this.setNumber(this.tagName, rest | Y_NE, data);
 				break;
 			case Z_DW:
-				this.setNumber(tagName, rest | X_DS, data);
+				this.setNumber(this.tagName, rest | X_DS, data);
 				break;
 			case Z_DE:
-				this.setNumber(tagName, rest | X_DN, data);
+				this.setNumber(this.tagName, rest | X_DN, data);
 				break;
 			case Z_UW:
-				this.setNumber(tagName, rest | X_US, data);
+				this.setNumber(this.tagName, rest | X_US, data);
 				break;
 			case Z_UE:
-				this.setNumber(tagName, rest | X_UN, data);
+				this.setNumber(this.tagName, rest | X_UN, data);
 				break;
 			case X_DN:
-				this.setNumber(tagName, rest | Z_DW, data);
+				this.setNumber(this.tagName, rest | Z_DW, data);
 				break;
 			case X_DS:
-				this.setNumber(tagName, rest | Z_DE, data);
+				this.setNumber(this.tagName, rest | Z_DE, data);
 				break;
 			case X_UN:
-				this.setNumber(tagName, rest | Z_UW, data);
+				this.setNumber(this.tagName, rest | Z_UW, data);
 				break;
 			case X_US:
-				this.setNumber(tagName, rest | Z_UE, data);
+				this.setNumber(this.tagName, rest | Z_UE, data);
 				break;
 			}
 		}
 
 		@Override
 		public void mirrorX(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~15);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~15);
 			switch ((byte)(number & 15)) {
 			case Y_NW:
-				this.setNumber(tagName, rest | Y_NE, data);
+				this.setNumber(this.tagName, rest | Y_NE, data);
 				break;
 			case Y_SW:
-				this.setNumber(tagName, rest | Y_SE, data);
+				this.setNumber(this.tagName, rest | Y_SE, data);
 				break;
 			case Y_NE:
-				this.setNumber(tagName, rest | Y_NE, data);
+				this.setNumber(this.tagName, rest | Y_NE, data);
 				break;
 			case Y_SE:
-				this.setNumber(tagName, rest | Y_SW, data);
+				this.setNumber(this.tagName, rest | Y_SW, data);
 				break;
 			case Z_DW:
-				this.setNumber(tagName, rest | Z_DE, data);
+				this.setNumber(this.tagName, rest | Z_DE, data);
 				break;
 			case Z_DE:
-				this.setNumber(tagName, rest | Z_DW, data);
+				this.setNumber(this.tagName, rest | Z_DW, data);
 				break;
 			case Z_UW:
-				this.setNumber(tagName, rest | Z_UE, data);
+				this.setNumber(this.tagName, rest | Z_UE, data);
 				break;
 			case Z_UE:
-				this.setNumber(tagName, rest | Z_UW, data);
+				this.setNumber(this.tagName, rest | Z_UW, data);
 				break;
 			}
 		}
 
 		@Override
 		public void mirrorZ(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~15);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~15);
 			switch ((byte)(number & 15)) {
 			case Y_NW:
-				this.setNumber(tagName, rest | Y_SW, data);
+				this.setNumber(this.tagName, rest | Y_SW, data);
 				break;
 			case Y_SW:
-				this.setNumber(tagName, rest | Y_NW, data);
+				this.setNumber(this.tagName, rest | Y_NW, data);
 				break;
 			case Y_NE:
-				this.setNumber(tagName, rest | Y_SE, data);
+				this.setNumber(this.tagName, rest | Y_SE, data);
 				break;
 			case Y_SE:
-				this.setNumber(tagName, rest | Y_NE, data);
+				this.setNumber(this.tagName, rest | Y_NE, data);
 				break;
 			case X_DN:
-				this.setNumber(tagName, rest | X_DS, data);
+				this.setNumber(this.tagName, rest | X_DS, data);
 				break;
 			case X_DS:
-				this.setNumber(tagName, rest | X_DN, data);
+				this.setNumber(this.tagName, rest | X_DN, data);
 				break;
 			case X_UN:
-				this.setNumber(tagName, rest | X_US, data);
+				this.setNumber(this.tagName, rest | X_US, data);
 				break;
 			case X_US:
-				this.setNumber(tagName, rest | X_UN, data);
+				this.setNumber(this.tagName, rest | X_UN, data);
 				break;
 			}
 		}
@@ -416,100 +416,100 @@ public class DefaultFMPManipulators {
 
 		@Override
 		public void rotate90(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~6);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~6);
 			switch ((byte)(number & 6)) {
 			case NW:
-				this.setNumber(tagName, rest | NE, data);
+				this.setNumber(this.tagName, rest | NE, data);
 				break;
 			case SW:
-				this.setNumber(tagName, rest | NW, data);
+				this.setNumber(this.tagName, rest | NW, data);
 				break;
 			case NE:
-				this.setNumber(tagName, rest | SE, data);
+				this.setNumber(this.tagName, rest | SE, data);
 				break;
 			case SE:
-				this.setNumber(tagName, rest | SW, data);
+				this.setNumber(this.tagName, rest | SW, data);
 				break;
 			}
 		}
 
 		@Override
 		public void rotate180(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~6);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~6);
 			switch ((byte)(number & 6)) {
 			case NW:
-				this.setNumber(tagName, rest | SE, data);
+				this.setNumber(this.tagName, rest | SE, data);
 				break;
 			case SW:
-				this.setNumber(tagName, rest | NE, data);
+				this.setNumber(this.tagName, rest | NE, data);
 				break;
 			case NE:
-				this.setNumber(tagName, rest | SW, data);
+				this.setNumber(this.tagName, rest | SW, data);
 				break;
 			case SE:
-				this.setNumber(tagName, rest | NW, data);
+				this.setNumber(this.tagName, rest | NW, data);
 				break;
 			}
 		}
 
 		@Override
 		public void rotate270(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~6);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~6);
 			switch ((byte)(number & 6)) {
 			case NW:
-				this.setNumber(tagName, rest | SW, data);
+				this.setNumber(this.tagName, rest | SW, data);
 				break;
 			case SW:
-				this.setNumber(tagName, rest | SE, data);
+				this.setNumber(this.tagName, rest | SE, data);
 				break;
 			case NE:
-				this.setNumber(tagName, rest | NW, data);
+				this.setNumber(this.tagName, rest | NW, data);
 				break;
 			case SE:
-				this.setNumber(tagName, rest | NE, data);
+				this.setNumber(this.tagName, rest | NE, data);
 				break;
 			}
 		}
 
 		@Override
 		public void mirrorX(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~6);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~6);
 			switch ((byte)(number & 6)) {
 			case NW:
-				this.setNumber(tagName, rest | NE, data);
+				this.setNumber(this.tagName, rest | NE, data);
 				break;
 			case SW:
-				this.setNumber(tagName, rest | SE, data);
+				this.setNumber(this.tagName, rest | SE, data);
 				break;
 			case NE:
-				this.setNumber(tagName, rest | NW, data);
+				this.setNumber(this.tagName, rest | NW, data);
 				break;
 			case SE:
-				this.setNumber(tagName, rest | SW, data);
+				this.setNumber(this.tagName, rest | SW, data);
 				break;
 			}
 		}
 
 		@Override
 		public void mirrorZ(NBTTagCompound data) {
-			long number = this.getNumber(tagName, data);
-			long rest =  number & (~6);
+			long number = this.getNumber(this.tagName, data);
+			long rest = number & (~6);
 			switch ((byte)(number & 6)) {
 			case NW:
-				this.setNumber(tagName, rest | SW, data);
+				this.setNumber(this.tagName, rest | SW, data);
 				break;
 			case SW:
-				this.setNumber(tagName, rest | NW, data);
+				this.setNumber(this.tagName, rest | NW, data);
 				break;
 			case NE:
-				this.setNumber(tagName, rest | SE, data);
+				this.setNumber(this.tagName, rest | SE, data);
 				break;
 			case SE:
-				this.setNumber(tagName, rest | NE, data);
+				this.setNumber(this.tagName, rest | NE, data);
 				break;
 			}
 		}
--- mw/fmp_manipulation/ModFMPManipulation.java
+++ mw/fmp_manipulation/ModFMPManipulation.java
@@ -11,13 +11,14 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid="mw.fmp_manipulation", name="ModFMPManipulation", version="1.0.0", dependencies = "required-after:MWLibrary;required-after:ForgeMultipart")
-@NetworkMod(clientSideRequired=false, serverSideRequired=true)
+//@NetworkMod(clientSideRequired=false, serverSideRequired=true)
 public class ModFMPManipulation {
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent postInitEvent) {
-		if (postInitEvent.getSide().isServer()) {
+		//if (postInitEvent.getSide().isServer()) {
+			FMLLog.info("Registering multipart manipulator - %d", MultipartProxy.block().blockID);
 			BlockManipulator.registerManipulator(MultipartProxy.block(), new ForgeMultiPartManipulator());
-		}
+		//}
 	}
 }
